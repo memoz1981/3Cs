d9e05b5d1416fbae4497b3e437ffa2e8cb7a743c
M.Z <zeynalov@gmail.com>
2024-03-02 18:23:21 +0400

    Added C# Hello World project

---
+++ 1_Hello_World/C#/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+//dotnet new console --language "C#" --framework "net7.0" --output HelloWorldApp
+//dotnet build
+//dotnet run
+Console.WriteLine("Hello World!!!");
