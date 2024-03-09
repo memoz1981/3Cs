81aa6d50565a606d796ebcce82f13441fa556c3a
M.Z <zeynalov@gmail.com>
2024-03-09 13:04:09 +0400

    C queue complete - re-factoring and infrastructure changes will be done in a separate branch

--- 2_Data_Structures/C/queue.c
+++ 2_Data_Structures/C/queue.c
@@ -13,6 +13,7 @@ struct Queue* InitializeQueue(int value)
 
     queue->value = value; 
     queue->next = NULL; 
+    
 
     return queue; 
 }
@@ -43,8 +44,10 @@ void FreeQueueWrapper(struct QueueWrapper* wrapper)
 }
 
 /* QUEUE FUNCTION IMPLEMENTATIONS */
+/*Enqueue function is O(n) - we need to add the element to the end of the queue*/
 void Enqueue(struct QueueWrapper* wrapper, struct Queue* item)
 {
+    printf("\nEnqueueing value %d\n", item->value); 
     struct Queue* iterator = wrapper->head; 
     wrapper->size++; 
 
@@ -62,50 +65,56 @@ void Enqueue(struct QueueWrapper* wrapper, struct Queue* item)
     iterator->next = item; 
 }
 
+/*
+Dequeue function is O(1) - we just return the head
+This is the main purpose of the wrapper - every time we dequeue the queue, we loose the 
+reference to the queue
+*/
 struct Queue* Dequeue(struct QueueWrapper* wrapper)
 {
+    printf("\nDequeueing...\n"); 
     if(wrapper->head == NULL)
+    {
+        printf("\nDequeued NULL.\n"); 
         return NULL; 
+    }
     
+    struct Queue* head = wrapper->head; 
     wrapper->size--; 
+    wrapper->head = wrapper->head->next;
 
-    struct Queue* iterator = wrapper->head; 
-
-    if(iterator->next == NULL)
-    {
-        wrapper->head = NULL;
-        wrapper->size = 0; 
-        return iterator; 
-    }
-
-    while(iterator->next->next != NULL)
-    {
-        iterator = iterator->next;  
-    }
-
-    struct Queue* lastElement = iterator->next; 
-    iterator->next = NULL; 
-    return lastElement; 
+    printf("\nDequeued value of %d.\n", head->value); 
+    return head->value; 
 }
 
+/*
+PeekQueue is similar to dequeue function - it only returns without removing the item 
+*/
 struct Queue* PeekQueue(struct QueueWrapper* wrapper)
 {
+    printf("\nPeeking...\n"); 
     if(wrapper->head == NULL)
+    {
+        printf("\nPeeked NULL.\n"); 
         return NULL; 
+    }
 
-    struct Queue* iterator = wrapper->head; 
-
-    while(iterator->next != NULL)
-        iterator = iterator->next; 
-    
-    return iterator; 
+    printf("\nDequeued value of %d.\n", wrapper->head->value); 
+    return wrapper->head->value; 
 }
 
+/*
+Returns the size of the queue
+Normally it's O(n) - but with the cost of one additional operation at each 
+enqueue/dequeue action and 4 bytes of memory to store the size
+we can return it with O(1) operation
+*/
 int QueueSize(struct QueueWrapper* wrapper)
 {
     return wrapper->size; 
 }
 
+/*Comments for QueueSize apply here*/
 int isQueueEmpty(struct QueueWrapper* wrapper)
 {
     return wrapper->size == 0; 
@@ -115,55 +124,58 @@ int isQueueEmpty(struct QueueWrapper* wrapper)
 void PrintSingleQueueItem(struct Queue* queue)
 {
     if(queue == NULL)
-        printf("Queue is NULL\n"); 
-    
-    int nextvalue = -1;
-    if(queue->next)
     {
-        nextvalue = queue->next->value; 
+        printf("Queue is NULL"); 
+        return; 
     }
 
-    printf("\nQueue value %d and address %p and next value is %d\n", queue->value, (void*)(&queue), nextvalue); 
+    printf("{value: %d, address: %p}",  queue->value, (void*)(&queue)); 
 }
 
 void PrintFullQueue(struct QueueWrapper* wrapper)
 {
-    printf("\nQueue Wrapper is being printed with count of %d\n", wrapper->size);
     struct Queue* iterator = wrapper->head;
 
-    while(iterator)
+    printf("\nQueue Wrapper: "); 
+    if(wrapper->head == NULL)
     {
-        PrintStack(iterator); 
+        printf("NULL\n"); 
+        return; 
+    }
+
+    for(int i=0; i<wrapper->size; i++)
+    {
+        PrintSingleQueueItem(iterator); 
+        printf(" -> "); 
         iterator = iterator->next;
     }
-    printf("\nQueue Wrapper print complete\n"); 
+
+    printf("\n"); 
 }
 
 /* QUEUE DEMO */
 void run_queue_test(void)
 {
-    printf("Defining queue array of 5 elements\n"); 
     struct Queue* queue[5];  
-
-    printf("Defining queue wrapper\n");
     struct QueueWrapper* wrapper; 
     
     //Initialization
-    printf("Queues are being initialized\n"); 
     for(int i=0; i<5; i++)
     {
         queue[i] = InitializeQueue(i); 
         PrintSingleQueueItem(queue[i]); 
     }
     
-    printf("Queue Wrapper is being initialized\n"); 
+    printf("\n4.Queue Wrapper is being initialized\n"); 
     wrapper = InitializeQueueWrapper(); 
     PrintFullQueue(wrapper); 
-    printf("Wrapper size: %d\n", Size(wrapper));
-    printf("Wrapper is empty: %d\n", isEmpty(wrapper)); 
-    Peek(wrapper); 
+    printf("\n5.Wrapper size: %d\n", Size(wrapper));
+    printf("\n6.Wrapper empty flag: %d\n", isEmpty(wrapper)); 
+
+    PeekQueue(wrapper); 
 
     //Actions
+    printf("\n7.Actions: \n"); 
     Enqueue(wrapper, queue[0]); 
     Enqueue(wrapper, queue[1]); 
     Enqueue(wrapper, queue[2]); 
@@ -173,10 +185,11 @@ void run_queue_test(void)
     struct Queue* dequeued2 = Dequeue(wrapper); 
 
     printf("Wrapper size: %d\n", Size(wrapper));
-    printf("Wrapper is empty: %d\n", isEmpty(wrapper)); 
-    Peek(wrapper); 
+    printf("Wrapper empty flag: %d\n", isEmpty(wrapper)); 
+
+    PeekQueue(wrapper); 
     
-    PrintWrapper(wrapper); 
+    PrintFullQueue(wrapper); 
     FreeQueueWrapper(wrapper); 
 
     for(int i=0; i<5; i++)
