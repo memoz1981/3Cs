1af3fc2c08ad034950375367a920a3d1e1bcd6be
M.Z <zeynalov@gmail.com>
2024-03-17 22:24:34 +0400

    Added stack C# interactive mode

--- 2_Data_Structures/C#/Stack.cs
+++ 2_Data_Structures/C#/Stack.cs
@@ -65,6 +65,70 @@ public class StackWrapper<T>
 
     public void RunStackInInteractiveMode()
     {
-        Console.WriteLine("Running stack in interactive mode..."); 
+        Console.Clear(); 
+        printf("\n\n\nINTERACTIVE STACK COMMANDS:\n"); 
+        Console.WriteLine("[c] - for clearing the screen\n");
+        Console.WriteLine("[r] - to print the stack\n");
+        Console.WriteLine("[e] - to exit\n");
+        Console.WriteLine("[+] - to push integer to the stack\n");
+        Console.WriteLine("[-] - to pop\n");
+        Console.WriteLine("[p] - to peek\n");
+        Console.WriteLine("[s] - to print stack count\n");
+
+        char ch = Console.ReadKey().KeyChar; 
+
+        var stackWrapper = new StackWrapper<int>(); 
+        int result;
+        do {
+            result = RunStackCycle(stackWrapper, ch); 
+        }
+        while(result != -1)
+    }
+
+    private int RunStackCycle(StackWrapper<int> wrapper, char ch)
+    {
+        if(ch == 'c')
+        {
+            Console.Clear(); 
+            return 0; 
+        }
+
+        if(ch == 'r')
+        {
+            Console.WriteLine(wrapper.ToString());
+            return 0; 
+        }
+
+        if(ch == 'e')
+            return -1; 
+
+        if(ch == '+')
+        {
+            Console.WriteLine("Enter an integer to add to the stack: ");
+            int result = Int32.Parse(Console.Readline()); 
+
+            wrapper.Push(value); 
+
+            return 0; 
+        }
+
+        if(ch == '-')
+        {
+            int popped = wrapper.Pop(); 
+
+            Console.WriteLine($"Popped value is {popped}"); 
+            return 0; 
+        }
+
+        if(ch == 'p')
+        {
+            int popped = wrapper.Peek(); 
+
+            Console.WriteLine($"Peeked value is {popped}"); 
+            return 0; 
+        }
+
+        if(ch == 's')
+            Console.WriteLine($"Stack size is {wrapper.Size}");
     }
 }
